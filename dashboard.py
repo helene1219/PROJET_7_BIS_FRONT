f608ccf6b2f159e2f39dc02fcc6f80d0cfac3e8e
Helene1219 <helene.wlache@wanadoo.fr>
2024-04-21 10:04:57 +0200

    Mise à jour dahsboard hors shap

--- dashboard.py
+++ dashboard.py
@@ -1,16 +1,31 @@
-import streamlit as st
+
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
 import plotly.express as px
-import shap
+
 import requests
+import shap
+import streamlit as st
+shap.initjs()
+from streamlit_shap import st_shap
+
+
 
 # Change the console encoding
 sys.stdout.reconfigure(encoding='utf-8')
 
+
+def dict_to_exp(dico: dict) -> shap._explanation.Explanation:
+    """
+    Convertir un dictionnaire en shap explanation
+    """
+    explanation = shap.Explanation(values=dico.values())
+    return explanation
 
 
 API_URL = "http://127.0.0.1:8000"
@@ -20,8 +35,9 @@ id_client = requests.get(f"{API_URL}/ids/").json()
 nb_credits=requests.get(f"{API_URL}/nb_credit/").json()
 rev_moy=requests.get(f"{API_URL}/rev_moyen/").json()
 credits_moy=requests.get(f"{API_URL}/credit_moyen/").json()
-df_age=requests.get(f"{API_URL}/age/").json()
-df_income=requests.get(f"{API_URL}/data_plot/").json()
+df_age=requests.get(f"{API_URL}/data_age/").json()
+df_income=requests.get(f"{API_URL}/data_income/").json()
+
             #######################################
                 # SIDEBAR
              #######################################
@@ -39,11 +55,11 @@ st.markdown(html_temp, unsafe_allow_html=True)
 st.sidebar.header("**INFORMATION GENERALE**")
 
 #Loading selectbox
-chk_id = st.sidebar.selectbox("Client ID", id_client)
+client_id = st.sidebar.selectbox("Client ID", id_client)
 
-data_id=requests.get(f"{API_URL}/data_client/{chk_id}").json()
-data_client=pd.DataFrame(data_id)
+response=requests.get(f"{API_URL}/data_client/{int(client_id)}").json()
 
+data_client=pd.DataFrame(response)
 #Loading general info
 
 
@@ -67,59 +83,61 @@ st.sidebar.text(credits_moy)
 st.header(" INFORMATION CLIENT SELECTIONNE ")
 
 if st.checkbox("AFFICHER LES INFORMATIONS SUR LE CLIENT ?",key="option1"):
-    #st.write(" SEXE: ", data_client["CODE_GENDER"].values[0])
-    #st.write(" AGE : {:.0f} ans".format(int(data_client["DAYS_BIRTH"]/-365)))
-    #st.write(" AGE : {:.0f} ans".data_client["DAYS_BIRTH"]/-365)
-    #st.write("SITUATION DE FAMILLE : ", data_client["NAME_FAMILY_STATUS"].values[0])
-    #st.write("NOMBRE D'ENFANT : {:.0f}".format(data_client["CNT_CHILDREN"].values[0]))
+    st.write(" SEXE: ", data_client["CODE_GENDER"].values[0])
+    st.write(" AGE : {:.0f} ans".format(int(data_client["DAYS_BIRTH"])))
+    st.write("SITUATION DE FAMILLE : ", data_client["NAME_FAMILY_STATUS"].values[0])
 
-        
+       
     #Age distribution plot
-    #data_age = load_age_population(data)
+    data_age=pd.DataFrame(df_age)
     fig, ax = plt.subplots(figsize=(10, 5))
-    sns.histplot(df_age, edgecolor = 'k', color="#D54773",bins=20)
-    #ax.axvline(int(-infos_client["DAYS_BIRTH"].values /365), color="black", linestyle='--')
+    sns.histplot(data_age["DAYS_BIRTH"], edgecolor = 'k', color="#D54773",bins=20)
+    ax.axvline(int(data_client["DAYS_BIRTH"].values), color="black", linestyle='--')
     ax.set(title='AGE CLIENT', xlabel='AGE', ylabel='')
     st.pyplot(fig)
 
 
-    #st.subheader("REVENU (EN €)")
-    #st.write("REVENU TOTAL : {:.0f}".format(infos_client["AMT_INCOME_TOTAL"].values[0]))
-    #st.write("MONTANT DU CREDIT : {:.0f}".format(infos_client["AMT_CREDIT"].values[0]))
-    #st.write("ANNUITE DU CREDIT : {:.0f}".format(infos_client["AMT_ANNUITY"].values[0]))
-    #st.write("MONTANT DU BIEN POUR LE CREDIT : {:.0f}".format(infos_client["AMT_GOODS_PRICE"].values[0]))
+    st.subheader("REVENU (EN €)")
+    st.write("REVENU TOTAL : {:.0f}".format(data_client["AMT_INCOME_TOTAL"].values[0]))
+    st.write("MONTANT DU CREDIT : {:.0f}".format(data_client["AMT_CREDIT"].values[0]))
+    st.write("ANNUITE DU CREDIT : {:.0f}".format(data_client["AMT_ANNUITY"].values[0]))
+    st.write("MONTANT DU BIEN POUR LE CREDIT : {:.0f}".format(data_client["AMT_GOODS_PRICE"].values[0]))
 
 
     #Income distribution plot
-    data_income = load_income_population(data)
+    data_income=pd.DataFrame(df_income)
     fig, ax = plt.subplots(figsize=(10, 5))
-    sns.histplot(data_income["AMT_INCOME_TOTAL"], edgecolor = 'k', color="#D54773", bins=10)
-    #ax.axvline(int(infos_client["AMT_INCOME_TOTAL"].values[0]), color="black", linestyle='--')
+    sns.histplot(data_income["AMT_INCOME_TOTAL"], edgecolor = 'k', color="#D54773", bins=20)
+    ax.axvline(int(data_client["AMT_INCOME_TOTAL"].values[0]), color="black", linestyle='--')
     ax.set(title='REVENU DES CLIENTS', xlabel='REVENU (EN €)', ylabel='')
     st.pyplot(fig)
-
-else:
     st.markdown("<i>…</i>", unsafe_allow_html=True)
     
     #Customer solvability display
     st.header(" ANALYSE CREDIT DEMANDE ")
-    prediction,statut = load_prediction(sample,X_test, chk_id, clf)
-    st.write(" PROBABLITE DE DEFAUT : {:.0f} %".format(round(float(prediction)*100, 2)))
-    st.write("STATUT DU CLIENT : ",statut)
+    statut=requests.get(f"{API_URL}/prediction/{int(client_id)}").json()
     
-    
-#Feature importance / description
-if st.checkbox("AFFICHER LES RESULTATS SUR LE CLIENT ?",key="Option2"):
-    nbligne=sample.loc[sample['SK_ID_CURR'] == int(chk_id)].index.item()
-    fig, ax = plt.subplots(figsize=(10, 10))
-    explainer = shap.Explainer(clf)
-    shap_values = explainer.shap_values(X_test)
-    shap_vals = explainer(X_test)
-    shap.waterfall_plot(shap_vals[nbligne][:, 0],show = False)
-    st.pyplot(fig)
-        
+    if statut==0:
+        profil="client risque - crédit non accordé"
+    else:
+        profil = "Client non risqué - crédit accordé"
+    st.write("STATUT DU CLIENT : ",profil)
+
 else:
-    st.markdown("<i>…</i>", unsafe_allow_html=True)    
+    st.markdown("<i>…</i>", unsafe_allow_html=True)  
+    
+
+
+#Feature importance / description
+#if st.checkbox("AFFICHER LES RESULTATS SUR LE CLIENT ?",key="Option2"):
+
+    #shap_id=requests.get(f"{API_URL}/shap/{int(client_id)}").json()
+    #exp = dict_to_exp(shap_id) 
+    #fig=shap.waterfall_plot(exp,show = False,max_display=10)
+    #st.pyplot(fig)
+    
+#else:
+    #st.markdown("<i>…</i>", unsafe_allow_html=True)    
     
     
     
