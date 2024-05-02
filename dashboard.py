7047af3f245254efba11b826679d357a82761c2b
Helene1219 <helene.wlache@wanadoo.fr>
2024-05-02 16:48:12 +0200

    autre shap

--- dashboard.py
+++ dashboard.py
@@ -144,8 +144,8 @@ if st.checkbox("AFFICHER LES RESULTATS SUR LE CLIENT ?",key="Option2"):
     st.write("TYPE SHAP : ",type_shap)
         
         
-    st_shap(shap.plots.waterfall(exp), height=600, width=1200)
-    
+    #st_shap(shap.plots.waterfall(exp), height=600, width=1200)
+    shap.waterfall_plot(exp,show = False)
 else:
     st.markdown("<i>…</i>", unsafe_allow_html=True)    
     
