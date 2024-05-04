c15cf5c270e31bf462a3484a8e5f9cc573b503b1
Helene1219 <helene.wlache@wanadoo.fr>
2024-05-04 14:41:22 +0200

    tet

--- dashboard.py
+++ dashboard.py
@@ -139,13 +139,15 @@ if st.checkbox("AFFICHER LES RESULTATS SUR LE CLIENT ?",key="Option2"):
 
     
     shap_id=requests.get(f"{API_URL}/shap/{int(client_id)}").json()
+    premier=ma_liste[0]
+    st.write("STATUT DU CLIENT : ",premier)
     #exp = dict_to_exp(shap_id) 
     #feat = list(feature.values())  
-    df_shap=pd.DataFrame(shap_id)
-    df_feat=pd.DataFrame(feature)    
+    #df_shap=pd.DataFrame(shap_id)
+    #df_feat=pd.DataFrame(feature)    
     #shap = list(shap_id.values())   
-    fig = plt.bar(x=df_shap, y=df_feat)
-    st.pyplot(fig)
+    #fig = plt.bar(x=df_shap, y=df_feat)
+    #st.pyplot(fig)
 else:
     st.markdown("<i>…</i>", unsafe_allow_html=True)    
     
