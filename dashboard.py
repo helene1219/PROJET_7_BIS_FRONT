510ce0a464f35ade5a77fe9d30fc59b5eef7287e
Helene1219 <helene.wlache@wanadoo.fr>
2024-05-04 13:01:56 +0200

    tet

--- dashboard.py
+++ dashboard.py
@@ -142,7 +142,7 @@ if st.checkbox("AFFICHER LES RESULTATS SUR LE CLIENT ?",key="Option2"):
     #exp = dict_to_exp(shap_id) 
     df = pd.DataFrame.from_dict(feature)
     df_shap = pd.DataFrame.from_dict(shap_id, orient='index')    
-    fig = plt.bar(df, df_shap,color="orange")
+    fig = plt.bar(x=df_shap, y=df,color="orange")
     st.pyplot(fig)
 else:
     st.markdown("<i>…</i>", unsafe_allow_html=True)    
