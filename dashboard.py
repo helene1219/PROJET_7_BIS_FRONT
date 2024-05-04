955f78020e0ec11c454f6f3a6c8299962336c84d
Helene1219 <helene.wlache@wanadoo.fr>
2024-05-04 15:08:53 +0200

    tet

--- dashboard.py
+++ dashboard.py
@@ -149,7 +149,7 @@ if st.checkbox("AFFICHER LES RESULTATS SUR LE CLIENT ?",key="Option2"):
     #feat = list(feature.values())  
     df_shap=pd.DataFrame.from_dict(shap_id, orient='index', columns=['Valeur']).reset_index()
     df_feature=pd.DataFrame(feature, columns=['Feature']).reset_index()
-    df=pd.concat([df_shap, df_feature])
+    df=pd.concat([df_shap, df_feature], axis=1)
     st.write("STATUT DU CLIENT : ",df_shap.head())    
     st.write("STATUT DU CLIENT : ",df_feature.head())  
     st.write("STATUT DU CLIENT : ",df.head())     
